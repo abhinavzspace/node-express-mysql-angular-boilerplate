d438f08cc82651482d4bbab347628ad50be062f4
abhinav <watsgoing@gmail.com>
2013-12-24 13:42:21 +0530

    views setup

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var app = express();
 
 // all environments
 //app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/app/views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
